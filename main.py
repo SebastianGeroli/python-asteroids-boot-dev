e60e96deddae72fa221f4ad1556410e0a0b4c8e4
SebastianGeroli <sebastiangeroli@gmail.com>
2026-08-31 00:34:38 +0000

    Add shots

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from logger import log_state, log_event
 from player import Player
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,12 +16,17 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Shot.containers =(shots, updatable, drawable) 
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable) 
+    AsteroidField.containers = (updatable)
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
     while True:
