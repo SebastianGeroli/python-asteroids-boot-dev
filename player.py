430872ece97077b0d4627eb0b451ecc77c63e410
SebastianGeroli <sebastiangeroli@gmail.com>
2026-08-31 00:38:13 +0000

    Add shot cooldown to player

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x:float, y:float):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
@@ -31,6 +32,7 @@ class Player(CircleShape):
 
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= dt
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
@@ -43,6 +45,9 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
